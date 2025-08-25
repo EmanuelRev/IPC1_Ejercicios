88f26b010d277fc14f5b114fddef40a51ca9408e
EmanuelRev <197363967+EmanuelRev@users.noreply.github.com>
2025-08-25 02:59:46 -0600

    Actualizando Repositorio Proyecto 1

---
+++ Proyecto_Inventario_Ropa/Proyecto_Inventario_de_Ropa/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class Main {
+     public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+         boolean salir = false;
+
+         int opcion;
+         while (!salir) {
+         //Solo Menu
+             System.out.println("-----Sistema de Inventario de Ropa ER-----");
+             System.out.println("---Menu Principal---");
+             System.out.println("SELECCIONE UNA OPCION PARA INICIAR");
+             System.out.println("1. Agregar Producto");
+             System.out.println("2. Buscar Producto");
+             System.out.println("3. Eliminar Producto");
+             System.out.println("4. Registrar Venta");
+             System.out.println("5. Generar Reportes");
+             System.out.println("6. Datos del Estudiante");
+             System.out.println("7. Bitacora");
+             System.out.println("8. Salir");
+
+             scanner.close();
+         }
+             opcion = scanner.nextInt();
+             scanner.nextLine();
+             switch (opcion) {
+                 case 1:
+                     System.out.println("n-> Opcion: Agregar Producto");
+                             //
+                     break;
+                 case 2:
+                     System.out.println("n-> Opcion: Buscar Producto");
+                     //
+                     break;
+                 case 3:
+                     System.out.println("n-> Opcion: Eliminar Producto");
+                     //
+                     break;
+                 case 4:
+                     System.out.println("n-> Opcion: Registrar Venta");
+                     //
+                     break;
+                 case 5:
+                     System.out.println("n-> Opcion: Generar Reportes");
+                     //
+                     break;
+                 case 6:
+                     System.out.println("n-> Opcion: Ver Datos del Estudiante");
+                     //
+                     break;
+                 case 7:
+                     System.out.println("n-> Opcion: Bitacora ");
+                     //
+                     break;
+                 case 8:
+                     System.out.println("n-> Opcion: Salir");
+                     //
+                     break;
+
+         }
+
+
+
+         }
+
+
+}
+
+
+
