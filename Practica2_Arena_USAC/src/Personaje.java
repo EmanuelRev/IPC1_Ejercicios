d45bd735b0556a9e6e62146f795734e1cf08eed7
EmanuelRev <197363967+EmanuelRev@users.noreply.github.com>
2025-09-21 21:43:38 -0600

    Agregando Practica 2 Clase main

---
+++ Practica2_Arena_USAC/src/Personaje.java
@@ -0,0 +1,16 @@
+public class Personaje {
+
+    private static int contador = 1;
+
+    private int id;
+    private String nombre;
+    private String arma;
+    private int vida;
+    private  int ataque;
+    private int velocidad;
+    private int agilidad;
+    private int defensa;
+    
+
+
+}
