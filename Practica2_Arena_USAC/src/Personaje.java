1601c655560b2be298dc2cfbccc5fdd66e2db440
EmanuelRev <197363967+EmanuelRev@users.noreply.github.com>
2025-09-22 00:02:42 -0600

    agregando constructor

--- Practica2_Arena_USAC/src/Personaje.java
+++ Practica2_Arena_USAC/src/Personaje.java
@@ -1,6 +1,6 @@
 public class Personaje {
 
-    private static int contador = 1;
+    private static int contadorId = 1;
 
     private int id;
     private String nombre;
@@ -11,6 +11,19 @@ public class Personaje {
     private int agilidad;
     private int defensa;
     
+   //esto es el construcctor para personajes
 
+    public Personaje(String nombre, String arma, int vida, int ataque, int velocidad, int agilidad, int defensa) {
+        this.id = contadorId;
+        contadorId++;
+        this.nombre = nombre;
+        this.arma = arma;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.velocidad = velocidad;
+        this.agilidad = agilidad;
+        this.defensa = defensa;
+
+    }
 
 }
